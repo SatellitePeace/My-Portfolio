697c3c2dc2bf9e4ed430909bb5b764ec62b14356
SatellitePeace <njokunneoma1999@gmail.com>
2025-01-06 11:49:06 +0100

    Section addition and overhauls

--- JS/index.js
+++ JS/index.js
@@ -1,59 +1,88 @@
 // // Toggle small screen navbar
-const menuIcon = document.getElementById("menu-icon")
-const navbar = document.querySelector(".navbar")
+const menuIcon = document.getElementById("menu-icon");
+const navbar = document.querySelector(".navbar");
 
 menuIcon.onclick = () => {
-    menuIcon.classList.toggle("bi-x-octagon")
-    navbar.classList.toggle("active")
-}
+  menuIcon.classList.toggle("bi-x-octagon");
+  navbar.classList.toggle("active");
+};
 // Change active nav links
-const sections = document.querySelectorAll("section")
-const navLinks = document.querySelectorAll("header nav a")
+const sections = document.querySelectorAll("section, .section");
+const navLinks = document.querySelectorAll("header nav a");
 
 window.onscroll = () => {
-    sections.forEach(section => {
-        let top = window.scrollY
-        let offset = section.offsetTop - 150
-        let height = section.offsetHeight
-        let id = section.getAttribute("id")
+  sections.forEach((section) => {
+    let top = window.scrollY;
+    let offset = section.offsetTop - 150;
+    let height = section.offsetHeight;
+    let id = section.getAttribute("id");
 
-        if (top >= offset && top < offset + height) {
-            navLinks.forEach(link => {
-                link.classList.remove("active")
-                document.querySelector('header nav a[href*=' + id + ']').classList.add("active")
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((link) => {
+        link.classList.remove("active");
+        document
+          .querySelector("header nav a[href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
+  });
+  // sticky navbar
+  const header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 100);
 
-            })
-        }
-    })
-    // sticky navbar
-    const header = document.querySelector("header")
-    header.classList.toggle("sticky", window.scrollY > 100)
+  // close small menu bar on scroll
+  menuIcon.classList.remove("bi-x-octagon");
+  navbar.classList.remove("active");
+};
 
-    // close small menu bar on scroll
-    menuIcon.classList.remove("bi-x-octagon")
-    navbar.classList.remove("active")
-}
-
-// // Type animation 
+// // Type animation
 const types = new Typed(".multiple-text", {
-    strings: ['Product Manager', "Frontend Developer", "Product Owner", "Project Manager", "Writer"],
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 1000,
-    loop: true
-})
+  strings: [
+    "Product Manager",
+    "Product Owner",
+    "Project Manager",
+    "Scrum Master",
+  ],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
 
 // scrool reveal text
 ScrollReveal({
-    reset: true,
-    distance: "80px",
-    duration: 2000,
-    delay: 200
-})
+  reset: true,
+  distance: "80px",
+  duration: 2000,
+  delay: 200,
+});
 
-ScrollReveal().reveal(".home-content, .about-content, .heading", { origin: "top" })
+ScrollReveal().reveal(".home-content, .about-content, .heading", {
+  origin: "top",
+});
 
-ScrollReveal().reveal(".home-img, .about-img, .services-container, .product, .contact-box", { origin: "bottom" })
+ScrollReveal().reveal(
+  ".home-img, .about-img, .services-container, .product, .contact-box",
+  { origin: "bottom" }
+);
+
+// SKILLS SECTION
+// filter sill with btns
+const skillsBtn = document.querySelectorAll(".skills-btn");
+skillsBtn.forEach((btn, ind) => {
+  btn.addEventListener("click", () => {
+    const skillDetails = document.querySelectorAll(".skill-detail");
+    skillsBtn.forEach((btn) => {
+      btn.classList.remove("active-skill");
+    });
+    btn.classList.add("active-skill");
+    // filter skill based on selected button
+    skillDetails.forEach((detail) => {
+      detail.classList.remove("active-skill");
+    });
+    skillDetails[ind].classList.add("active-skill");
+  });
+});
 
 // PORTFOLIO FILTER
 const list = document.querySelectorAll(".list")
@@ -80,71 +109,77 @@ for (let i = 0; i < list.length; i++) {
         }
     })
 }
-// lightmode
-const lightModeIcon = document.getElementById("lightMode-icon")
+// LIGHTMODE TOGGLE
+const lightModeIcon = document.getElementById("lightMode-icon");
 lightModeIcon.onclick = () => {
-    lightModeIcon.classList.toggle("bi-cloud-moon")
-    document.body.classList.toggle("light-mode")
-}
+  lightModeIcon.classList.toggle("bi-cloud-moon");
+  document.body.classList.toggle("light-mode");
+};
+// CONTACT
+
+// CONTACT END
 // COPY EMAIL
 document.addEventListener("DOMContentLoaded", () => {
-    const copyButton = document.getElementById("copyButton");
-    const copyTextDiv = document.getElementById("copyText");
-    const copyMessage = document.getElementById("copyMessage");
+  const copyButton = document.getElementById("copyButton");
+  const copyTextDiv = document.getElementById("copyText");
+  const copyMessage = document.getElementById("copyMessage");
 
-    // Add click event listener to the button
-    copyButton.addEventListener("click", () => {
-        const textToCopy = copyTextDiv.textContent;
-        // Copy the text to the clipboard
-        navigator.clipboard.writeText(textToCopy)
-            .then(() => {
-                // Show a success message
-                copyMessage.textContent = "Email copied to clipboard!";
-                copyMessage.classList.add("success");
-                copyMessage.classList.remove("error");
-                setTimeout(() => {
-                    copyMessage.textContent = "";
-                }, 2000);
-            })
-            .catch(err => {
-                console.error('Failed to copy email: ', err);
-                copyMessage.textContent = "Failed to copy text!";
-                copyMessage.classList.add("error");
-                copyMessage.classList.remove("success");
-                setTimeout(() => {
-                    copyMessage.textContent = "";
-                }, 2000);
-            });
-    });
+  // Add click event listener to the button
+  copyButton.addEventListener("click", () => {
+    const textToCopy = copyTextDiv.textContent;
+    // Copy the text to the clipboard
+    navigator.clipboard
+      .writeText(textToCopy)
+      .then(() => {
+        // Show a success message
+        copyMessage.textContent = "Email copied to clipboard!";
+        copyMessage.classList.add("success");
+        copyMessage.classList.remove("error");
+        setTimeout(() => {
+          copyMessage.textContent = "";
+        }, 2000);
+      })
+      .catch((err) => {
+        console.error("Failed to copy email: ", err);
+        copyMessage.textContent = "Failed to copy text!";
+        copyMessage.classList.add("error");
+        copyMessage.classList.remove("success");
+        setTimeout(() => {
+          copyMessage.textContent = "";
+        }, 2000);
+      });
+  });
 });
 
 // COPYRIGHT DATE
-const copyDate = document.querySelector("small")
-copyDate.innerText = new Date().getFullYear()
+const copyDate = document.querySelector("small");
+copyDate.innerText = new Date().getFullYear();
 // SUBMIT FORM TO GOOGLE SHEETS
 
-const scriptURL = 'https://script.google.com/macros/s/AKfycbz7mZWJsctvUlMiYi5w4N89063sAozUrc2yqZjNEqrv7MoZAX5moruWHLGHD26N_CXd/exec'
-const form = document.forms['submit-to-google-sheet']
-const msg = document.getElementById("form-message")
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbz7mZWJsctvUlMiYi5w4N89063sAozUrc2yqZjNEqrv7MoZAX5moruWHLGHD26N_CXd/exec";
+const form = document.forms["submit-to-google-sheet"];
+const msg = document.getElementById("form-message");
 
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      msg.innerHTML = `<i class="bi bi-check-circle-fill"></i> Message sent successfully!!`;
+      msg.style.color = "green";
+      setTimeout(function () {
+        msg.innerHTML = "";
+      }, 5000);
+      form.reset();
+    })
+    .catch((error) => {
+      msg.style.color = "red";
+      msg.innerHTML = `<i class="bi bi-x-square-fill"></i> Message not sent, Please try again`;
+      setTimeout(function () {
+        msg.innerHTML = "";
+      }, 5000);
+      form.reset();
+    });
+});
 
-form.addEventListener('submit', e => {
-    e.preventDefault()
-    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
-        .then(response => {
-            msg.innerHTML = `<i class="bi bi-check-circle-fill"></i> Message sent successfully!!`
-            msg.style.color = "green"
-            setTimeout(function () {
-                msg.innerHTML = ""
-            }, 5000)
-            form.reset()
-        })
-        .catch(error => {
-            msg.style.color = "red"
-            msg.innerHTML = `<i class="bi bi-x-square-fill"></i> Message not sent, Please try again`
-            setTimeout(function () {
-                msg.innerHTML = ""
-            }, 5000)
-            form.reset()
-        })
-})
+// SPLIDE
